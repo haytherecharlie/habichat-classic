250a06b6f492f290a3e07d7b449710c7c2c597ca
Charlie Hay <haytrix@Charlies-MacBook-Pro.local>
2020-03-28 22:22:20 -0400

    feat(hc): updated register

--- functions/src/controllers/createUser.ts
+++ functions/src/controllers/createUser.ts
@@ -7,7 +7,7 @@ const authCreateUserAsync = async user => {
   try {
     return await auth.createUser({ ...user, disabled: false, emailVerified: false })
   } catch (err) {
-    throw errors['V006']
+    throw err.code || errors['V006']
   }
 }
 
@@ -15,23 +15,24 @@ const dbCreateUserAsync = async (ref, user) => {
   try {
     return await ref.set({ ...user, joined: timestamp() })
   } catch (err) {
-    throw errors['V007']
+    throw err.code || errors['V007']
   }
 }
 
 const createUser = async (req, res) => {
   try {
-    const { email, password, displayName, community = 'pyNBzg3V742S5v8gWfRB' } = req.body
-    const [photoURL] = await Promise.all([getAvatarAsync(), vName(displayName), vEmail(email), vPass(password)])
+    const { firstName, lastName, email, password } = req.body
+    const community = 'pyNBzg3V742S5v8gWfRB'
+    const [photoURL] = await Promise.all([getAvatarAsync(), vName(firstName), vName(lastName), vEmail(email), vPass(password)])
     const ref = db.collection(`users`).doc()
     const [user] = await Promise.all([
-      authCreateUserAsync({ displayName, email, password, photoURL, uid: ref.id }),
-      dbCreateUserAsync(ref, { displayName, photoURL, community })
+      authCreateUserAsync({ displayName: firstName, email, password, photoURL, uid: ref.id }),
+      dbCreateUserAsync(ref, { firstName, lastName, photoURL, community })
     ])
     return res.status(200).json({ code: 'auth/register-success', data: { user } })
   } catch (err) {
     console.log(err)
-    return res.status(400).json({ code: err, data: {} })
+    return res.status(400).json({ code: err })
   }
 }
 
