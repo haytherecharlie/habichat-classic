47a0f520dc56f12109a164a8749b7c41a8f53bd1
Charlie Hay <haytrix@Charlies-MacBook-Pro.local>
2020-04-05 21:23:58 -0400

    feat(hc): avatar community

--- application/src/screens/Community.tsx
+++ application/src/screens/Community.tsx
@@ -2,21 +2,20 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { TouchableOpacity, Text } from 'react-native'
 import { auth } from 'services/firebase'
+import Avatar from 'atoms/Avatar'
 import CenterLayout from 'layouts/CenterLayout'
 import SubHeader from 'components/SubHeader'
 import MessageList from 'components/MessageList'
 import Compose from 'components/Compose'
 
 const Community = () => {
-  const { displayName } = useSelector(s => s.user.account)
+  const { displayName, photoURL } = useSelector(s => s.user.account)
   return (
     <CenterLayout>
+      <Avatar source={{ uri: photoURL }} onPress={() => auth.signOut()} />
       <Text>{displayName}</Text>
-      <TouchableOpacity onPress={() => auth.signOut()}>
-        <Text>Sign Out</Text>
-      </TouchableOpacity>
-      {/* <SubHeader />
-      <MessageList />
+      {/* <SubHeader /> */}
+      {/* <MessageList />
       <Compose /> */}
     </CenterLayout>
   )
