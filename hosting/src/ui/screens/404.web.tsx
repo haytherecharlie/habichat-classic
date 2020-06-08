ae841a092a75ead4b6f1d3c776412a37b5b1959d
Charlie Hay <robertcharleshay@outlook.com>
2020-06-07 21:05:22 -0400

    feat(hc): 404 page

--- hosting/src/ui/screens/404.web.tsx
+++ hosting/src/ui/screens/404.web.tsx
@@ -8,13 +8,13 @@ const _404 = () => {
   return (
     <PageLayout page="404" crawl={false} style={{ alignItems: 'center', justifyContent: 'center' }}>
       <img src="/image/spilt-milk.png" style={{ width: `80%`, maxWidth: 400, margin: '0 auto' }} alt="spilt milk" />
-      <Text size="L" text="Oops! We all make mistakes..." style={{ textAlign: 'center', marginTop: 10 }} bold unique />
-      <Text size="L" text="Page not found!" style={{ textAlign: 'center' }} bold unique />
+      <Text size="M" text="Oops! We all make mistakes..." style={{ textAlign: 'center', marginTop: 10 }} bold unique />
+      <Text size="M" text="Page not found!" style={{ textAlign: 'center' }} bold unique />
       <Link href="/">
         <Text
           size="S"
           text="Return to homepage"
-          style={{ marginTop: 10, textAlign: 'center', color: theme.BRAND_COLOR, fontWeight: 900 }}
+          style={{ marginTop: 10, textAlign: 'center' }}
           unique
         />
       </Link>
