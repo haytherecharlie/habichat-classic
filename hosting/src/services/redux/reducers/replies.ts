19154c58b0b8bb2967870561e4bc94b81e18c854
Charlie Hay <robertcharleshay@outlook.com>
2020-06-11 21:26:53 -0400

    feat(hc): reply default state

--- hosting/src/services/redux/reducers/replies.ts
+++ hosting/src/services/redux/reducers/replies.ts
@@ -1,6 +1,6 @@
 import * as A from 'services/redux/actions'
 
-const defaultState = null
+const defaultState = {}
 
 const repliesReducer = (state = defaultState, action) => {
   switch (action.type) {
